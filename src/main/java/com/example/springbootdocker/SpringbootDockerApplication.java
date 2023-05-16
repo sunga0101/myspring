43bde5111bf7dabe3da8ded30fb2a2f3001aebec
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-05-16 15:51:08 +0900

    [feat] mapping index.html

--- src/main/java/com/example/springbootdocker/SpringbootDockerApplication.java
+++ src/main/java/com/example/springbootdocker/SpringbootDockerApplication.java
@@ -2,9 +2,14 @@ package com.example.springbootdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootDockerApplication {
+
+    @RequestMapping("/")
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDockerApplication.class, args);
