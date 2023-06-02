0e1db18bd825b569ad7d56ded8a9c5ed3b488d80
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-06-02 14:39:59 +0900

    [feat] Spring jpa sqlite에 데이터 저장

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -19,4 +24,17 @@ public class DemoApplication {
 	public String hello(@RequestParam(value = "name",defaultValue = "world") String name) {
 		return "Hello " + name;
 	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return args -> {
+			repository.save(new Customer("leo", "aa"));
+
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+		};
+	}
+
 }
